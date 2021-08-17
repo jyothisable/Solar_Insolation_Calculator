4ad9d14162feb897d4ce0ab059551d7f66df3a7c
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-08-18 01:26:20 +0530

    main function done

--- app.py
+++ app.py
@@ -10,4 +10,18 @@ using grass gis r.sun module
 import grass.script as gs
 import os
 
-def calculate(dem,)
+def calculate(dem_input,day,time):
+    '''
+    inputs
+    dem_input: string name of DEM file 
+    day:integer [1-365]
+    time:integer [1-24]
+    output : Solar insolation as raster (.tif) file
+    '''
+    gs.run_command('g.region', raster=dem_input)
+    os.chdir('data/input_DEMs'); 
+    gs.run_command('r.sun', elevation=dem_input, horizon_basename=(dem_input)+'_horangle', horizon_step=30, aspect=(dem_input)+'_aspect.dem',
+                   slope=(dem_input)+'_slope.dem', glob_rad=(dem_input)+'_solar_insolation', day=day, time=time, linke_value=1, nprocs=6, overwrite=True)
+    os.chdir('../output'); 
+    gs.run_command('r.out.gdal', input=(dem_input)+'_solar_insolation',
+                   output=(dem_input)+'_output'+'.tif', type='Float64', overwrite=True)
