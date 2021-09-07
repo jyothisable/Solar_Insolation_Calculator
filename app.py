e3bfe3fe51d764da0e0b2f5b118a15a2e74abae4
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-09-08 04:47:18 +0530

    ouput to output folder with proper naming

--- app.py
+++ app.py
@@ -9,30 +9,42 @@ using grass gis r.sun module
 """
 
 import os
+from pathlib import Path
 import csv
 import grass.script as gs
 os.chdir(os.path.dirname(__file__))
 
 
-def findSolarInsolation(path='data/input_DEMs/IIT_Bbs_DEM.tif', day=30, time=11):
-    gs.run_command('r.in.gdal', input=path,
-                   output='DEM', overwrite=True)
-    gs.run_command('r.horizon', elevation='DEM', step=7.5,
-                   output='horangle')
-    gs.run_command('r.slope.aspect', elevation='DEM',
-                   aspect='aspect.dem', slope='slope.dem', overwrite=True)
-    gs.run_command('r.sun', elevation='DEM', horizon_basename='horangle',
-                   horizon_step=7.5, aspect='aspect.dem', slope='slope.dem',
-                   glob_rad='global_rad', day=30, time=11, nprocs=6, linke_value=5,
-                   albedo_value=0.4, overwrite=True)
-    gs.run_command('r.univar', map='global_rad',
-                   output='data/cache/stats_cache.csv', separator='comma', overwrite=True, flags='te')
+def findSolarInsolation(path='data/input_DEMs/IIT_Bbs_DEM.tif', day=30, time=12):
+    files = Path('data/input_DEMs').glob('*.tif')
+    for file in files:
+        gs.run_command('r.in.gdal', input=file,
+                       output='DEM', overwrite=True)
+        gs.run_command('r.horizon', elevation='DEM', step=7.5,
+                       output='horangle')
+        gs.run_command('r.slope.aspect', elevation='DEM',
+                       aspect='aspect.dem', slope='slope.dem', overwrite=True)
+        # for this data set => need to change #todo find a module to calc this
+        solar_time = time - 1
+        gs.run_command('r.sun', elevation='DEM', horizon_basename='horangle',
+                       horizon_step=7.5, aspect='aspect.dem', slope='slope.dem',
+                       glob_rad='global_rad', day=day, time=solar_time, nprocs=6, linke_value=5,
+                       albedo_value=0.4, overwrite=True)
 
-    with open('data/cache/stats_cache.csv', newline='') as cache_csv:
-        lastLine = cache_csv.read().splitlines()[-1]
-    with open('data/output/stats.csv', 'a') as output_csv:
-        output_csv.write("\n")
-        output_csv.writelines(lastLine)
+        # output results stats into CSV (can't append directly)
+        gs.run_command('r.univar', map='global_rad',
+                       output='data/cache/stats_cache.csv', separator='comma', overwrite=True, flags='te')
+
+        # copy and append to a permanent stats.csv file
+        with open('data/cache/stats_cache.csv', newline='') as cache_csv:
+            lastLine = cache_csv.read().splitlines()[-1]
+        fileName = os.path.basename(file).split('.')[0]
+        with open('data/output/' + fileName + '_stats.csv', 'a') as output_csv:
+            output_csv.write("\n")
+            output_csv.writelines(lastLine)
+
+        gs.run_command('r.out.gdal', input='global_rad',
+                       output='data/output/'+fileName + '_D'+str(day)+'_H'+str(time)+'.tif', type='Float64', overwrite=True)
 
 
 findSolarInsolation()
