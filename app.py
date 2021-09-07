87ed1ea09f62ce8c1c3d98dbc81563acc22574cb
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-09-08 02:37:35 +0530

    writing function

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
-#!/usr/bin/env conda run -n MTP python
+#!/usr/bin/env conda run -n MTP python3
 # =============================================================================
 # Created By  : Athul Jyothis
 # Created Date: 18-08-2021 00:17:21
 # =============================================================================
 """
-The module has been build for calculating the solar insolation of a region using DEM (digital elevation modlel)
+The module has been build for calculating the solar insolation of a region using DEM (digital elevation model)
 using grass gis r.sun module
 """
-import grass.script as gs
-import os
 
-def calculate(dem_input,day,time):
-    '''
-    inputs
-    dem_input: string name of DEM file 
-    day:integer [1-365]
-    time:integer [1-24]
-    output : Solar insolation as raster (.tif) file
-    '''
-    gs.run_command('g.region', raster=dem_input)
-    os.chdir('data/input_DEMs'); 
-    gs.run_command('r.sun', elevation=dem_input, horizon_basename=(dem_input)+'_horangle', horizon_step=30, aspect=(dem_input)+'_aspect.dem',
-                   slope=(dem_input)+'_slope.dem', glob_rad=(dem_input)+'_solar_insolation', day=day, time=time, linke_value=1, nprocs=6, overwrite=True)
-    os.chdir('../output'); 
-    gs.run_command('r.out.gdal', input=(dem_input)+'_solar_insolation',
-                   output=(dem_input)+'_output'+'.tif', type='Float64', overwrite=True)
+import os
+import grass.script as gs
+os.chdir(os.path.dirname(__file__))
+
+
+def findSolarInsolation(path='data/input_DEMs/IIT_Bbs_DEM.tif'):
+    gs.run_command('r.in.gdal', input=path,
+                   output='DEM', overwrite=True)
+    gs.run_command('r.horizon ', elevation='DEM', step=7.5,
+                   output='horangle', overwrite=True)
+    gs.run_command('r.slope.aspect', elevation='DEM',
+                   aspect='aspect.dem', slope='slope.dem', overwrite=True)
+    gs.run_command('r.sun', elevation='DEM', horizon_basename='horangle',
+                   horizon_step=7.5, aspect='aspect.dem', slope='slope.dem',
+                   glob_rad='global_rad', day=30, time=11, nprocs=6, linke_value=5,
+                   albedo_value=0.4, overwrite=True)
+    gs.run_command('r.univar2', map='global_rad', output='data/output')
+# r.horizon elevation = desert step = 30 output = horangle - -overwrite
+# r.slope.aspect elevation = desert aspect = aspect.dem slope = slope.dem - -overwrite
+# r.sun elevation = desert horizon_basename = horangle horizon_step = 30 aspect = aspect.dem slope = slope.dem glob_rad = global_rad day = 30 time = 11 nprocs = 4 linke_value = 5 albedo_value = 0.4 - -overwrite
+# r.univar global_rad
+# d.mon wx0
+# d.rast.leg global_rad
+# r.out.gdal input = global_rad output = desert_output.tif - -overwrite
