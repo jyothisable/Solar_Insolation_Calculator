d8489f89e2d4d7a914b5827636e389ab9ec1e5d6
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-09-08 03:55:05 +0530

    append to a new csv

--- app.py
+++ app.py
@@ -9,22 +9,31 @@ using grass gis r.sun module
 """
 
 import os
+import csv
 import grass.script as gs
 os.chdir(os.path.dirname(__file__))
 
 
-def findSolarInsolation(path='data/input_DEMs/IIT_Bbs_DEM.tif'):
+def findSolarInsolation(path='data/input_DEMs/IIT_Bbs_DEM.tif', day=30, time=11):
     gs.run_command('r.in.gdal', input=path,
                    output='DEM', overwrite=True)
-    gs.run_command('r.horizon ', elevation='DEM', step=7.5,
-                   output='horangle', overwrite=True)
+    gs.run_command('r.horizon', elevation='DEM', step=7.5,
+                   output='horangle')
     gs.run_command('r.slope.aspect', elevation='DEM',
                    aspect='aspect.dem', slope='slope.dem', overwrite=True)
     gs.run_command('r.sun', elevation='DEM', horizon_basename='horangle',
                    horizon_step=7.5, aspect='aspect.dem', slope='slope.dem',
                    glob_rad='global_rad', day=30, time=11, nprocs=6, linke_value=5,
                    albedo_value=0.4, overwrite=True)
-    gs.run_command('r.univar2', map='global_rad', output='data/output')
+    gs.run_command('r.univar', map='global_rad',
+                   output='data/cache/stats_cache.csv', separator='comma', overwrite=True, flags='te')
+    with open('data/cache/stats_cache.csv', newline='') as cache_csv:
+        lastLine = csv.DictReader(cache_csv)[-1]
+    with open('data/output/stats.csv', 'a') as output_csv:
+        output_csv.writelines(lastLine)
+
+
+findSolarInsolation()
 # r.horizon elevation = desert step = 30 output = horangle - -overwrite
 # r.slope.aspect elevation = desert aspect = aspect.dem slope = slope.dem - -overwrite
 # r.sun elevation = desert horizon_basename = horangle horizon_step = 30 aspect = aspect.dem slope = slope.dem glob_rad = global_rad day = 30 time = 11 nprocs = 4 linke_value = 5 albedo_value = 0.4 - -overwrite
