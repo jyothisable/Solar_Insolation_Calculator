12fb64d824f9528f4f7d74bfb5f15f943c0faef5
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-09-21 08:54:24 +0530

    comments added

--- app.py
+++ app.py
@@ -26,23 +26,23 @@ def findSolarInsolation(day, time):
     '''
     file = 'data/input_DEMs/desert_dem.tif'
     gs.run_command('r.in.gdal', input=file,output='DEM', overwrite=True)
+    # convert time format 
     t = int(time*100)-550
     min = str(int(((t%100)/100)*60))
     if min =='0': min = '00'
     time_UTC = '0'+str(t//100) + min
-    # print(time_UTC)
-    gs.run_command('r.in.gdal', input='data/input_clouds/3DIMG_'+day+'JAN2020_'+time_UTC+'_L2B_CMK_CMK.tif',output='cloud', overwrite=True, flags='o')
-    gs.run_command('r.mapcalc.simple', a='cloud',expression='result = A/3',output='cloud_cf',overwrite=True)
+    # gs.run_command('r.in.gdal', input='data/input_clouds/3DIMG_'+day+'JAN2020_'+time_UTC+'_L2B_CMK_CMK.tif',output='cloud', overwrite=True, flags='o')
+    # gs.run_command('r.mapcalc.simple', a='cloud',expression='result = A/3',output='cloud_cf',overwrite=True)
     gs.run_command('r.horizon', elevation='DEM', step=7.5,output='horangle')
     gs.run_command('r.slope.aspect', elevation='DEM',
                     aspect='aspect.dem', slope='slope.dem', overwrite=True)
     # solar time only applicable for this data set => need to change #todo find a module to calc this
-    solar_time = time - 0.8
+    solar_time = time - 1.4
     gs.run_command('r.sun', elevation='DEM', horizon_basename='horangle',
                     horizon_step=7.5, aspect='aspect.dem', slope='slope.dem',
                     glob_rad='global_rad', day=day, time=solar_time, nprocs=6, linke_value=1,
-                    albedo_value=0.3,coeff_bh='cloud_cf', overwrite=True)
-                    # albedo_value=0.3, overwrite=True)
+                    # albedo_value=0.3,coeff_bh='cloud_cf', overwrite=True)
+                    albedo_value=0.3, overwrite=True)
 
     res = 0.03618888887999958  # specific to this case 0.0352666666399999
     gs.run_command('r.resamp.interp', input='global_rad',
@@ -65,7 +65,8 @@ def saveOutput(inputFileName,fileNameInGrass,day,time):
     with open('data/output/' + inputFileName + '_stats.csv', 'a') as output_csv:
         if os.stat('data/output/' + inputFileName + '_stats.csv').st_size == 0:
             output_csv.writelines(
-                'day,time,non_null_cells,null_cells,min,max,range,mean,mean_of_abs,stddev,variance,coeff_var,sum,sum_abs,first_quart,median,third_quart,perc_90')
+                """day,time,non_null_cells,null_cells,min,max,range,mean,mean_of_abs,stddev,variance,coeff_var,sum,sum_abs,
+                first_quart,median,third_quart,perc_90""")
         output_csv.write("\n")
         output_csv.writelines(str(day)+','+str(time)+','+lastLine)
 
@@ -74,12 +75,12 @@ def saveOutput(inputFileName,fileNameInGrass,day,time):
 
 
 # specify range of day [1-365 int] and time [24h float]
-for day in range(1, 32):
+for day in range(1, 2):
     if day < 10:day ='0'+ str(day)
     else: day =str(day)
     for time in range(22, 31):  # 11am to 3pm
         t = time/2
-        # print('this is day ',day,' and time ',t)
+        # skip non existant time
         if day =='05' and t == 11.5: 
             continue 
         elif day =='16' and t== 11.0:
